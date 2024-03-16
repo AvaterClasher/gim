1ebf793241c646f2993993ae074275067e55112c
AvaterClasher <avater.clasher@gmail.com>
2024-03-16 18:58:53 +0530

    feat: :sparkles: Raw mode for loop

--- gim.go
+++ gim.go
@@ -5,6 +5,10 @@ import (
 )
 
 func main() {
+	buffer := make([]byte, 1)
+	for cc,err := os.Stdin.Read(buffer); err == nil && cc==1; cc,err = os.Stdin.Read(buffer) {
+		//blank
+	}
 	os.Exit(0)
 }
 
